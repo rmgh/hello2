67290dde9752c17f6ffb8f571da88ae51158450d
rm <minowaryohei@minowa-no-MacBook-Air.local>
2015-03-10 11:48:58 +0900

    Initial Commit

---
+++ hello2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello2
+//
+//  Created by minowaryohei on 2015/03/10.
+//  Copyright (c) 2015年 rm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hello2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello2
+//
+//  Created by minowaryohei on 2015/03/10.
+//  Copyright (c) 2015年 rm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
